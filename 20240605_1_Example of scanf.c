c9d90d345a27bb4474e574655374754d108dd744
GuanYuxi <143797026+DrGuanYuxi@users.noreply.github.com>
2024-06-12 10:45:10 +0800

    Add files via upload

---
+++ 20240605_1_Example of scanf.c
@@ -0,0 +1,13 @@
+# define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+
+int main(void)
+{
+	int i, j, k;
+
+	scanf("%d %d %d", &i, &j, &k);
+
+	printf("i = %d, j = %d, k = %d\n", i, j, k);
+
+	return 0;
+}
