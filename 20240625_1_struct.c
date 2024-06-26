00b8077e755766279bb4c3083cb35596443c5a87
GuanYuxi <143797026+DrGuanYuxi@users.noreply.github.com>
2024-06-26 10:30:38 +0800

    Add files via upload

---
+++ 20240625_1_struct.c
@@ -0,0 +1,27 @@
+# define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+
+struct Student
+{
+	int age;
+	float score;
+	char sex;
+};
+
+int main(void)
+{
+	struct Student st1 = {80, 66.6f, 'F'};
+	struct Student st2;
+	st2.age = 10;
+	st2.score = 66.6f;
+	st2.sex = 'F';
+	printf("%d %f %c\n", st2.age, st2.score, st2.sex);
+
+	struct Student* pst = &st2;
+	printf("%d %f %c\n", pst->age, pst->score, pst->sex);
+	//pst->age will translate to (*pst).age in the computer
+
+
+
+	return 0;  
+}
